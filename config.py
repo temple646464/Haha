1f82afd4e66b4f5cf6d80bb6782e903ab570fef3
temple646464 <Temple646464@gmail.com>
2025-04-27 19:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "28748671"
+api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
+bot_token = "8086953920:AAGYBOwM4ysQ0bZKrOJn0IADQE439nz1t2E"
